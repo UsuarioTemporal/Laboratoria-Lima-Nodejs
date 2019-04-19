693652d8952259a975c7537644198d20d148bc9b
UsuarioTemporal <thomtwd@gmail.com>
2019-04-18 19:22:23 -0500

    Aclaraciones

--- introduction_2/app.js
+++ introduction_2/app.js
@@ -12,3 +12,6 @@ let argv = process.argv
 apps(argv[2],argv[3])
 .then(console.log)
 .catch(console.log)
+
+
+// $ node app.js '../db/' 'js' , para que funcione sin problemas se debe de encontrar en el archvio donde se encuentra la app
