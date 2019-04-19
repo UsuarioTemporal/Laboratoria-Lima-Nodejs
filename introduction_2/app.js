9ef28c00d27ece390e24f7993b805a83b8a2f12a
UsuarioTemporal <thomtwd@gmail.com>
2019-04-18 19:17:39 -0500

    Actualizacion

--- introduction_2/app.js
+++ introduction_2/app.js
@@ -5,12 +5,10 @@ const apps = async(path,extention)=>{
     const data = await dir(path)
     // console.log(data)
     let newArr = data.filter(item=>item.endsWith(extention))
-    Promise.all(newArr.map(item => file(path, item)))
-      .then(console.log)
-      .catch(console.error)
+    return Promise.all(newArr.map(item => file(path, item)))
 }
 
 let argv = process.argv
 apps(argv[2],argv[3])
-.then(response=>console.log(response))
-.catch(err=>console.log(err))
+.then(console.log)
+.catch(console.log)
