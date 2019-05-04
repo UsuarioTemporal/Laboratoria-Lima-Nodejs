080c40a0d44ed720eeb0a2431ab4e5aec72e1388
UsuarioTemporal <thomtwd@gmail.com>
2019-05-04 13:46:46 -0500

    Inmutabilidad de los datos

--- excercise/functional.js
+++ excercise/functional.js
@@ -2,3 +2,8 @@ const reverse = _string => _string.split(' ').reverse().join(' ')
 console.log(reverse('hola amigos'))
 
 // Data inmutability
+// https://jarroba.com/mutables-e-inmutables/
+// Todos lo datos primitivos son inmutables
+/**
+ * Se trabajará con estructuras de datos inmutables
+ */
